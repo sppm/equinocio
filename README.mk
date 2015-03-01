0ee99cbed01174734d2ac78864b34d2c220de66b
Renato CRON <renato.cron@gmail.com>
2015-03-01 13:48:03 -0300

    novo readme

---
+++ README.mk
@@ -0,0 +1,19 @@
+Publicações SPPM
+================
+
+Esse foi o repositório público de documentos dos equinócios da São Paulo Perl Mongers.
+
+No momento, o local novo para os artigos fica no repositório do SPPM-Web:
+
+[https://github.com/sppm/SPPM-Web](https://github.com/sppm/SPPM-Web)
+
+Você pode enviar as publicações em:
+
+[https://github.com/sppm/SPPM-Web/tree/beta/web/root/src/equinocio](https://github.com/sppm/SPPM-Web/tree/beta/web/root/src/equinocio)
+e
+[https://github.com/sppm/SPPM-Web/tree/beta/web/root/src/artigos](https://github.com/sppm/SPPM-Web/tree/beta/web/root/src/artigos)
+
+Fazer um pull-request, enviar o conteudo para alguem importar para o banco de dados.
+
+Se desejar fazer isso por você mesmo, faça o deploy do banco, depois execute o script `script/scan_article_to_json.pl` e em seguida o `script/json_to_db.pl`
+
